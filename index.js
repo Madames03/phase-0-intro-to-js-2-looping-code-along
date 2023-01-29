6a483e0d2bff57cb4c111aa09792c81990501222
Marino Adames <marinoadames85@gmail.com>
2023-01-29 04:27:48 -0500

    My head is Loopy LOL

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+const nameOnCards = ["Gaudalupe", "Ollie", "Aki"];
+const message = "surprise"
+
+function writeCards(nameOnCards, message) {
+    let newMessage = []
+    for (let card = 0; card < nameOnCards.length; card++) {
+        newMessage.push("Thank you, " + nameOnCards[card] +"," + " for the wonderful " + message + " gift!")
+    }
+    return newMessage
+}
+
+const letsCount = 10
+function countDown(letsCount) {
+    let starting = -1
+    while(starting < letsCount) {
+        console.log(letsCount--)
+    }
+}
